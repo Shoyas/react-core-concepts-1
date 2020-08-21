f6ca24114182ce88ae2bb20d5a0789152f5ab612
Shoyas <md.nasiruddin4067@gmail.com>
2020-08-21 22:34:47 +0600

    first problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 function App() {
+
+  const products= [
+    {name: 'Adobe Photoshop', price: '$40'},
+    {name: 'Adobe premium pro', price: '$50'},
+    {name: 'Adobe illustrator', price: '$20'},
+    {name: 'Adobe PDF Reader', price: '$5'},
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Person heroName="Bappa Raz" heroineName="Moyuri" ></Person>
+        <Person heroName="Jashim" heroineName="Shabana"></Person>
+        <Person heroName="Sakib Khan" heroineName="Bubli"></Person>
+        <Person heroName="Arefin Shuvo" heroineName="Mahya Mahi"></Person> */}
+
+        {/* <Product name={products[0].name} price={products[0].price}></Product> */}
+
+        <Product product={products[0]}></Product>
+        <Product ></Product>
+        <Product></Product>
+        <Product></Product>
+        
+
       </header>
     </div>
   );
 }
+
+// function Person(props){
+//   const style={
+//     width: '500px',
+//     border: '5px double yellow',
+//     margin: '10px',
+//     padding: '10px',
+//   }
+//   return (
+//     <div style={style}>
+//       <h1>Hero: {props.heroName}</h1>
+//       <p>Heroine: {props.heroineName}</p>
+//     </div>
+//   )
+// }
+
+function Product(props){
+  const productsStyle={
+    
+    margin: '10px 10px',
+    padding: '10px',
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: '#A8A8A8',
+    float: 'left',
+  }
+  console.log(props.product);
+  return (
+    <div style={productsStyle}>
+      <h4>Name: {props.product.name}</h4>
+      <p>Price: {props.product.price}</p>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
 
 export default App;
