81785255a68e6c35ea23d5a416ce0aea1c85ba6f
Shoyas <md.nasiruddin4067@gmail.com>
2020-08-22 13:17:32 +0600

    module 32 is complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 function App() {
+  
+  const nayok= ['Razzak', 'Dipojol', 'Amit Hasan', 'Pritom Hasan', 'Elias Kanchon', 'Batabi Lebu']
 
   const products= [
     {name: 'Adobe Photoshop', price: '$40'},
@@ -11,6 +13,9 @@ function App() {
     {name: 'Adobe illustrator', price: '$20'},
     {name: 'Adobe PDF Reader', price: '$5'},
   ]
+
+  // const productNames = products.map( (element) => element.price)
+  // console.log(productNames);
 
   return (
     <div className="App">
@@ -22,10 +27,37 @@ function App() {
 
         {/* <Product name={products[0].name} price={products[0].price}></Product> */}
 
-        <Product product={products[0]}></Product>
-        <Product ></Product>
-        <Product></Product>
-        <Product></Product>
+        {/* <ul>
+          <li>{nayok[0]}</li>
+          <li>{nayok[1]}</li>
+          <li>{nayok[2]}</li>
+          <li>{nayok[3]}</li>
+          <li>{nayok[4]}</li>
+          <li>{nayok[5]}</li>
+        </ul> */}
+
+        <Counter></Counter>
+
+        <Users></Users>
+        
+
+        <ol>
+          {
+            nayok.map(person => <li>{person}</li>)
+          }
+
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ol>
+
+
+        
+
+        <Product productItem={products[0]}></Product>
+        <Product productItem={products[1]}></Product>
+        <Product productItem={products[2]}></Product>
+        <Product productItem={products[3]}></Product>
         
 
       </header>
@@ -48,6 +80,43 @@ function App() {
 //   )
 // }
 
+function Users(){
+  const [user, setUser] = useState([]);
+  
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+      setUser(data);
+    })
+  })
+
+  return (
+    <div>
+      <h1>Dynamic Users : {user.length}</h1>
+      <ul>
+          {
+            user.map( element => <li>{element.name}'s Email: {element.email}</li>)
+          }
+        </ul>
+
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(10);
+  const countMeterIn = (event) => setCount(count + 1);
+  const countMeterDec = (event) => setCount(count - 1);
+  return (
+    <div>
+      <h3>Count : {count}</h3>
+      <button onClick={countMeterIn}>Increase</button>
+      <button onClick={countMeterDec}>Decrease</button>
+    </div>
+  )
+}
+
 function Product(props){
   const productsStyle={
     
@@ -58,11 +127,11 @@ function Product(props){
     backgroundColor: '#A8A8A8',
     float: 'left',
   }
-  console.log(props.product);
+  console.log(props.productItem); 
   return (
     <div style={productsStyle}>
-      <h4>Name: {props.product.name}</h4>
-      <p>Price: {props.product.price}</p>
+      <h4>Name: {props.productItem.name}</h4>
+      <p>Price: {props.productItem.price}</p>
       <button>Buy now</button>
     </div>
   )
